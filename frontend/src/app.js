98df37db7d20d464fb1e0c9e32959e3069926e2d
haerang <gofkdvjvl@naver.com>
2020-07-15 16:03:09 +0900

    투두리스트 렌더링하는 함수 구현, 각 투두리스트 만들어주는 함수 구현

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,9 +1,9 @@
 const css = require('./scss/main.scss');
 const variable = require('./scss/_variables.scss');
-// import './scss/main.scss';
-// import './scss/_variables.scss';
 import './normalize.css';
+import { renderTodo } from './modules/renderItem.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  
+  // todo:todo의 모든 아이템을 순환하는 반복문 추가하기
+  renderTodo('progress', '렌더링테스트', '안녕하세요~', 'haerang');
 });
