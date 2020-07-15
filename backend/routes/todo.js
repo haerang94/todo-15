1076ae4ddba75bc6df5fbfbf7e9cab855a8a7d93
parkjihwanjay <cc6656@naver.com>
2020-07-15 17:43:37 +0900

    [api/post/todolist]
    * POST /api/todolist 완성
    * title, status, author가 존재하는지 기본적인 validate
    * error가 발생하면 error code와 error message
    * postman으로 테스트 완성

--- backend/routes/todo.js
+++ backend/routes/todo.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const router = express.Router();
 
-const getTodoCallback = require('../functions/todoCallback.js');
+const {getTodoCallback, postTodoCallback} = require('../functions/todoCallback.js');
 // api 서버
 
 router.get('/', getTodoCallback);
-
-
+router.post('/', postTodoCallback);
 
 module.exports = router;
