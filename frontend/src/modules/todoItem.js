522d947fac3132840c8a41e8007d41b9d880a4d8
haerang <gofkdvjvl@naver.com>
2020-07-15 17:08:09 +0900

    css 수정

--- frontend/src/modules/todoItem.js
+++ frontend/src/modules/todoItem.js
@@ -2,7 +2,7 @@ export function todoItem(title, content, author) {
   const eachTodo = `<li class="todo-item">
       <div class="todo-item-header">
           <div class="todo-item-part1">
-              <div class="todo-item-logo">logo</div>
+              <div class="todo-item-logo"><i class="fa fa-list-alt" aria-hidden="true" fa-lg></i></div>
               <div class="todo-item-title">${title}</div>
           </div>
           <i class="fa fa-times-circle-o todo-item-part2 fa-lg" aria-hidden="true"></i>
