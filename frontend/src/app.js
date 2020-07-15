9b95d7b9bb1657ed3f0f25dab3885d8d1bcb2d46
haerang <gofkdvjvl@naver.com>
2020-07-15 16:45:18 +0900

    새로운 컬림 추가하는 함수 수정

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,10 +2,10 @@ const css = require('./scss/main.scss');
 const variable = require('./scss/_variables.scss');
 import './normalize.css';
 import { renderTodo } from './modules/renderItem.js';
-import { renderContainer } from './modules/renderContainer';
+import { addContainer } from './modules/addContainer';
 
 window.addEventListener('DOMContentLoaded', () => {
   // todo:todo의 모든 아이템을 순환하는 반복문 추가하기
   renderTodo('progress', '렌더링테스트', '안녕하세요~', 'haerang');
-  renderContainer();
+  addContainer();
 });
--- frontend/src/modules/addContainer.js
+++ frontend/src/modules/addContainer.js
@@ -1,6 +1,6 @@
 import { containerItem } from './containerItem.js';
 
-export function renderContainer() {
+export function addContainer() {
   const main = document.querySelector('main');
   const newContainer = containerItem();
   main.insertAdjacentHTML('beforeend', newContainer);
