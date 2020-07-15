fc760a64220a5f66f848b6a3f68b1d5b1fc705e7
haerang <gofkdvjvl@naver.com>
2020-07-15 17:53:44 +0900

    불필요한 파일 제거

--- frontend/src/modules/renderItem.js
+++
@@ -1,7 +0,0 @@
-import { todoItem } from './todoItem.js';
-
-export function renderTodo(status, title, content, author) {
-  const ul = document.querySelector(`#${status}`);
-  const item = todoItem(title, content, author);
-  ul.insertAdjacentHTML('beforeend', item);
-}
