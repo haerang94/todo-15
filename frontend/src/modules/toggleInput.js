798df0bcbc2656ce223400f38f1c5311b5953477
haerang <gofkdvjvl@naver.com>
2020-07-16 15:17:52 +0900

    토글 함수 리팩토링 및 기타 오타 수정함
    토글 함수의 노드를 이동하는 것을 split으로 id를 찾는 것으로 변경하고 기타 수정하지 않았던 것들을 추가 수정했습니다.

--- frontend/src/modules/toggleInput.js
+++ frontend/src/modules/toggleInput.js
@@ -1,17 +1,11 @@
 export function toggle() {
-  const togglePlusBtn = document.querySelectorAll('.fa-plus-square-o');
-
-  togglePlusBtn.forEach((btn) => {
-    btn.addEventListener('click', (e) => {
-      //   console.log(e.target);
-      const columnId = e.target.parentNode.parentNode.parentNode.parentNode.id;
-      const inputContainer = document.getElementById(`input-${columnId}`);
-      //   console.log(inputContainer);
-      if (inputContainer.classList.contains('hidden')) {
-        inputContainer.classList.remove('hidden');
-      } else {
-        inputContainer.classList.add('hidden');
-      }
-    });
+  document.addEventListener('click', (e) => {
+    const id = e.target.id.split('-')[2];
+    const inputContainer = document.getElementById(`input-${id}`);
+    if (inputContainer.classList.contains('hidden')) {
+      inputContainer.classList.remove('hidden');
+    } else {
+      inputContainer.classList.add('hidden');
+    }
   });
 }
