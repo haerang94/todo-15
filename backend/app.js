3bdb0cbaa922292657dcf3001582e6cca0055e7e
parkjihwanjay <cc6656@naver.com>
2020-07-16 14:19:44 +0900

    localhost:9000에 대해 cors 설정
    * app.use로 전역에 대해 localhost:9000에 cors 설정

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const path = require("path");
+const cors = require('cors');
 
 const todoRouter = require("./routes/todo.js");
 const userRouter = require('./routes/user.js');
@@ -9,7 +10,9 @@ var app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-
+app.use(cors({
+  origin : 'http://localhost:9000'
+}))
 app.use("/api/todo", todoRouter);
 app.use("/api/user", userRouter);
 
