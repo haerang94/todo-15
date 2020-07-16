703a0f1ddc78bc0f1f74b152df122023a9a92362
parkjihwanjay <cc6656@naver.com>
2020-07-16 14:25:09 +0900

    origin 주소 변수화

--- backend/app.js
+++ backend/app.js
@@ -5,14 +5,15 @@ const cors = require('cors');
 const todoRouter = require("./routes/todo.js");
 const userRouter = require('./routes/user.js');
 
+const allowOrigin = 'http://localhost:9000';
 var app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-app.use(cors({
-  origin : 'http://localhost:9000'
-}))
+
+app.use(cors({origin : allowOrigin}))
+
 app.use("/api/todo", todoRouter);
 app.use("/api/user", userRouter);
 
