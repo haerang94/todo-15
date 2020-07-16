7b583f4ef46f715ebe3538f7d07c50118f569b76
haerang <gofkdvjvl@naver.com>
2020-07-16 20:57:01 +0900

    페이지 css디자인 수정 및 add버튼 disable디자인 추가

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,29 +2,35 @@ const css = require('./scss/main.scss');
 const variable = require('./scss/_variables.scss');
 import './normalize.css';
 import { toggle } from './modules/toggleInput.js';
-// import Container from './modules/container.js';
 import Container from './modules/container.js';
 import { getFetchManger, postFetchManger } from './modules/fetchManger.js';
 import Item from './modules/item';
 // import Item from './modules/item.js';
 
 window.addEventListener('DOMContentLoaded', () => {
-  getFetchManger('http://localhost:3000/api/todo')
+  getFetchManger('/api/todo/count')
+    .then((res) => res.json())
+    .then((data) => {
+      const col = new Container();
+      for (let i = 0; i < data; i++) {
+        col.addContainer(i + 1);
+      }
+    })
+    .catch((e) => console.log(e));
+
+  getFetchManger('/api/todo')
     .then((res) => res.json())
     .then((data) => {
       const item = new Item();
       for (let i = 0; i < data.length; i++) {
-        let id = data[i].groupId.split('-')[1];
-        item.addItem(id, data[i].title, data[i].content, data[i].author);
+        let groupId = data[i].groupId.split('-')[1];
+        item.addItem(groupId, data[i].title, data[i].content, data[i].author); // todo:id추가하는 것 작성 필요
       }
-      toggle();
     })
     .catch((e) => console.log(e));
-
-  // todo:todo의 모든 아이템을 순환하는 반복문 추가하기
-  //칼럼 추가하는 함수
-  //   new Container().addContainer('4');
 });
+
+toggle();
 
 //ex)
 
--- frontend/src/modules/container.js
+++ frontend/src/modules/container.js
@@ -9,17 +9,17 @@ export default class Container {
                  <p class="todo-container-header-title">새로 추가된 todo컬럼</p>
              </div>
              <div class="todo-container-part2">
-                 <i id="add-btn-${id} class="fa fa-plus-square-o fa-lg" aria-hidden="true"></i>
-                 <i class="fa fa-ellipsis-h fa-lg" aria-hidden="true"></i>
+                 <i id="add-btn-${id}" class="fa fa-plus-square-o fa-2x" aria-hidden="true"></i>
+                 <i class="fa fa-ellipsis-h fa-2x" aria-hidden="true"></i>
              </div>
          </div>
      </header>
      <article>
-         <ul id="input-${id} toggle">
+         <ul id="input-${id}" class="hidden toggle">
              <li class="add-item">
                  <textarea style="width:100%; height:70px;" placeholder="Enter a note"></textarea>
                  <div class="add-item-btns">
-                     <input class="add-item-btn" type="button" value="Add">
+                     <input class="add-item-btn" type="button" value="Add" disabled>
                      <input class="add-item-btn" type="button" value="Cancel">
                  </div>
              </li>
@@ -30,8 +30,8 @@ export default class Container {
      </section>`;
   }
   addContainer(id) {
-    const article = document.querySelector('article');
+    const aside = document.querySelector('aside');
     const newContainer = this.makeContainer(id);
-    article.insertAdjacentHTML('beforebegin', newContainer);
+    aside.insertAdjacentHTML('beforebegin', newContainer);
   }
 }
