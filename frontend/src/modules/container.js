db14a939d3e94c4ccc138b36d5e7cd725d19d433
haerang <gofkdvjvl@naver.com>
2020-07-17 00:29:20 +0900

    투두리스트 항목컨텐츠에 긴 문장이 올 경우 넘어가는 현상 수정

--- frontend/src/modules/container.js
+++ frontend/src/modules/container.js
@@ -5,7 +5,7 @@ export default class Container {
      <header>
          <div class="todo-container-header">
              <div class="todo-container-part1">
-                 <div class="num-of-todos">0</div>
+                 <div class="num-of-todos"></div>
                  <p class="todo-container-header-title">새로 추가된 todo컬럼</p>
              </div>
              <div class="todo-container-part2">
@@ -17,7 +17,7 @@ export default class Container {
      <article>
          <ul id="input-${id}" class="hidden toggle">
              <li class="add-item">
-                 <textarea style="width:100%; height:70px;" placeholder="Enter a note"></textarea>
+                 <textarea style="width:95%; height:70px;" placeholder="Enter a note"></textarea>
                  <div class="add-item-btns">
                      <input class="add-item-btn" type="button" value="Add" disabled>
                      <input class="add-item-btn" type="button" value="Cancel">
