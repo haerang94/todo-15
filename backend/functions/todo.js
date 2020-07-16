7409e497a43a3fbfb5f6282211063463bcc88bc8
parkjihwanjay <cc6656@naver.com>
2020-07-16 10:43:43 +0900

    [todo/refactor] 미들웨어 추가
    * POST /api/todo에 validate하는 로직을 미들웨어로 추가
    
    * todoCallback.js -> todo.js로 파일 이름 변경

---
+++ backend/functions/todo.js
@@ -0,0 +1,45 @@
+const getTodoDB = require('../db/TodoList/getTodoList.js');
+const insertTodo = require('../db/TodoList/insertTodo.js');
+
+const statusCode = require('../utils/statusCode.js');
+const errorMessage = require('../utils/errorMessage.js');
+
+async function getTodoCallback(req, res){
+    try{
+        const result = await getTodoDB();
+        const todoList = result[0];
+        return res.status(statusCode.OK).json(todoList);
+    }catch(e){
+        return res.status(statusCode.DB_ERROR).send(errorMessage.DB_ERROR);
+    }
+}
+
+function validateTodo(req, res, next){
+    const todo = req.body;
+
+    let check = true;
+
+    if(!todo.title) check = false;
+    if(!todo.status) check = false;
+    if(!todo.author) check = false;
+
+    if(!check) return res.status(statusCode.BAD_REQUEST).send(errorMessage.BAD_REQUEST);
+    
+    next();
+}
+
+async function postTodoCallback(req, res){
+    try{
+        const result = await insertTodo(todo);
+        return res.sendStatus(statusCode.OK);
+    }catch(e){
+        console.log(e);
+        return res.status(statusCode.DB_ERROR).send(errorMessage.DB_ERROR);
+    }
+
+}
+
+
+
+module.exports = {getTodoCallback, postTodoCallback, validateTodo};
+
