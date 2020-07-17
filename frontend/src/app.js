b6bd48bb028036018ab8149b0462148cd3abda0b
parkjihwanjay <cc6656@naver.com>
2020-07-17 11:23:24 +0900

    [front/refactor] main.js 추가
    * app.js의 로직을 Main class를 만들어서 분리
    * Main은 처음 렌더링 될때 필요한 로직과 속성을 모아둠
    * Item, Container, Main은 component의 성격을 가지고 있으므로 Components 폴더를 새로 만들어 위치시킴

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,67 +2,11 @@ const css = require('./scss/main.scss');
 const variable = require('./scss/_variables.scss');
 import './normalize.css';
 import { toggle } from './modules/toggleInput.js';
-import Container from './modules/container.js';
-import { getFetchManger, postFetchManger } from './modules/fetchManger.js';
-import Item from './modules/item';
-// import Item from './modules/item.js';
+import Main from './components/main.js';
+import 'regenerator-runtime/runtime';
 
-let num_of_columns;
-window.addEventListener('DOMContentLoaded', () => {
-  getFetchManger('/api/todo/count')
-    .then((data) => {
-      num_of_columns = data;
-      console.log(num_of_columns);
-      const col = new Container();
-      for (let i = 0; i < data; i++) {
-        col.addContainer(i + 1);
-      }
-    })
-    .catch((e) => console.log(e));
-
-  getFetchManger('/api/todo')
-    .then((data) => {
-      const item = new Item();
-      for (let i = 0; i < data.length; i++) {
-        item.addItem(
-          data[i].id,
-          data[i].groupId,
-          data[i].title,
-          data[i].content,
-          data[i].author,
-        );
-      }
-    })
-    .then(() => temp())
-    .catch((e) => console.log(e));
+window.addEventListener('DOMContentLoaded', async () => {
+  const main = new Main(document.querySelector('main'));
+  await main.init();
+  toggle();
 });
-
-// 각 컬럼의 투두리스트 개수를 구하기 위한 임시함수
-const temp = () => {
-  for (let i = 0; i < num_of_columns; i++) {
-    const num_of_items = document.querySelector(`#todoList-${i + 1}`);
-    const result = document.querySelector(`.num-of-todos-${i + 1}`);
-    //text 제외한 자식 li태그 개수
-    result.innerText = num_of_items.childNodes.length - 1;
-  }
-};
-
-toggle();
-
-//ex)
-
-// 1.POST
-
-//const addButton = document.querySelector('.add-item-btn');
-// console.log(addButton);
-// const body = {
-//   title: 'asdf',
-//   content: 'asdf',
-//   author: 'asdf',
-//   groupId: 'todo-3',
-//   idx: 11,
-// };
-// addButton.addEventListener(
-//   'click',
-//   postFetchManger('http://localhost:3000/api/todo', body),
-// );
