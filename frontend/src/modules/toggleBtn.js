61d4dbbc35736ee150888df308e6804f251e17fa
haerang <gofkdvjvl@naver.com>
2020-07-17 13:51:44 +0900

    cancel버튼으로아이템 추가창 닫기 구현

--- frontend/src/modules/toggleBtn.js
+++ frontend/src/modules/toggleBtn.js
@@ -1,18 +1,10 @@
 export function toggleBtn() {
   document.addEventListener('input', (e) => {
-<<<<<<< HEAD
-    if (e.target.getAttribute('id').substring(0, 8) === 'textarea') {
-      const id = e.target.getAttribute('id').substring(9);
-      const btn = document.querySelector(`#input-btn-${id}`);
-      if (e.target.value.length !== 0) {
-        btn.removeAttribute('disabled');
-=======
     if (e.target.type === 'textarea') {
       const textarea = e.target;
       const addBtn = textarea.nextElementSibling.firstElementChild;
       if (textarea.value.length !== 0) {
         addBtn.removeAttribute('disabled');
->>>>>>> c0291fef4327d036e53533ecba3f772059de464b
       } else {
         addBtn.setAttribute('disabled', 'true');
       }
--- frontend/src/modules/toggleInput.js
+++ frontend/src/modules/toggleInput.js
@@ -2,11 +2,21 @@ export function toggleInput() {
   document.addEventListener('click', (e) => {
     if (!e.target.classList.contains('fa-plus')) return;
     const id = e.target.id.split('-')[2];
-    const inputContainer = document.getElementById(`input-todo-${id}`);
-    if (inputContainer.classList.contains('hidden')) {
-      inputContainer.classList.remove('hidden');
-    } else {
-      inputContainer.classList.add('hidden');
-    }
+    toggleHidden(id);
   });
+
+  document.addEventListener('click', (e) => {
+    if (e.target.id.substr(0, 10) !== 'cancel-btn') return;
+    const id = e.target.id.substr(11);
+    toggleHidden(id);
+  });
+}
+
+function toggleHidden(id) {
+  const inputContainer = document.getElementById(`input-todo-${id}`);
+  if (inputContainer.classList.contains('hidden')) {
+    inputContainer.classList.remove('hidden');
+  } else {
+    inputContainer.classList.add('hidden');
+  }
 }
