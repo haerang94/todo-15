4e697597e7a2d64ff78234c55dba394f34cf3a8d
parkjihwanjay <cc6656@naver.com>
2020-07-21 10:57:16 +0900

    addTodo & deleteTodo 리팩토링
    ## addTodo
    * 공통 로직은 따로 util로 만듬
      * updateCount, splitText의 경우 다른데서도 쓰기 때문에 utils 폴더 안에 모듈화
    
    ## deleteTodo
    * updateCount를 이용해 리팩토링

--- frontend/src/modules/deleteTodo.js
+++ frontend/src/modules/deleteTodo.js
@@ -1,14 +1,16 @@
 import { deleteFetchManager } from './fetchManger';
-
+import { updateCount } from './utils/updateCount.js';
 export default async function deleteTodo(e) {
   if (e.target.dataset.method !== 'delete') return;
   const deleteItem = e.target.closest('li');
   const id = deleteItem.id;
+  const ul = e.target.closest('ul');
   try {
     const result = await deleteFetchManager(`/api/todo/${id}`);
-    if (result.status !== 200) return alert('다시 시도해주세요');
     deleteItem.remove();
+    updateCount(ul);
   } catch (e) {
+    console.log(e);
     alert('다시 시도해주세요');
   }
 }
