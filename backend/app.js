6f6b15ea938afd64da7e86d6b7783d4d4d1f3fa1
parkjihwanjay <cc6656@naver.com>
2020-07-22 14:48:29 +0900

    column router 추가

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ const path = require("path");
 const cors = require("cors");
 
 const todoRouter = require("./routes/todo.js");
+const columnRouter = require("./routes/column.js");
 // const userRouter = require("./routes/user.js");
 
 const allowOrigin = "http://localhost:9000";
@@ -14,8 +15,8 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(cors({ origin: allowOrigin }));
 
-app.use("/api/todo", todoRouter);
-// app.use("/api/user", userRouter);
+app.use("/api/todos", todoRouter);
+app.use("/api/columns", columnRouter);
 
 app.listen(3000, () => {
   console.log("running in http://localhost:3000");
