f21b3b43b04f3cd2d009d473dbb26eb789ea388c
haerang <gofkdvjvl@naver.com>
2020-07-23 22:52:39 +0900

    컬럼 삭제 안된다아아아

--- backend/db/TodoList/deleteTodoList.js
+++ backend/db/TodoList/deleteTodoList.js
@@ -2,7 +2,7 @@ const promisePool = require("../connection.js");
 const { todoList } = require("../tableList.js");
 
 function deleteTodoList(groupId) {
-  const query = `DELETE FROM ${todoList} where groupId=${groupId}`;
+  const query = `DELETE FROM TODOLIST where groupId=${groupId}`;
   console.log(query);
   return promisePool.execute(query);
 }
