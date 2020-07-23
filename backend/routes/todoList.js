b2e51f5921773cfcd84e47699de81f2b8e28a134
parkjihwanjay <cc6656@naver.com>
2020-07-23 16:56:17 +0900

    todolist를 추가하는 api 생성
    * request로 groupId, groupTitle를 보내면 TODOLIST에 저장하는 api 생성
    * postman으로 테스트 완료
    * 쿼리문 완성

--- backend/routes/todoList.js
+++ backend/routes/todoList.js
@@ -4,8 +4,10 @@ const router = express.Router();
 const {
   getTodoListCallback,
   patchTodoListsCallback,
+  postTodoListCallback,
 } = require("../functions/todoList.js");
 router.get("/", getTodoListCallback);
+router.post("/", postTodoListCallback);
 router.patch("/:groupId", patchTodoListsCallback);
 
 module.exports = router;
