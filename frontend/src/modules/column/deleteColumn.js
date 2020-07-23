5156de27881f3c8cf2729bc898a7cb8bbe361d52
haerang <gofkdvjvl@naver.com>
2020-07-23 17:49:22 +0900

    컬럼 삭제 프론트 함수 구현

--- frontend/src/modules/column/deleteColumn.js
+++ frontend/src/modules/column/deleteColumn.js
@@ -1,1 +1,7 @@
-export default function deleteColumn() {}
+export default function deleteColumn() {
+  document.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('fa-times')) return;
+
+    e.target.closest('section').remove();
+  });
+}
