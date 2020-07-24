b527415326b46680b6cc9e30b8660b304a0598a9
parkjihwanjay <cc6656@naver.com>
2020-07-24 13:02:01 +0900

    배포 설정

--- frontend/webpack.common.js
+++ frontend/webpack.common.js
@@ -52,27 +52,6 @@ module.exports = {
     new CleanWebpackPlugin(),
     new webpack.ProgressPlugin(),
   ],
-  optimization: {
-    splitChunks: {
-      chunks: 'async',
-      minSize: 30000,
-      maxSize: 0,
-      minChunks: 1,
-      maxAsyncRequests: 6,
-      maxInitialRequests: 4,
-      automaticNameDelimiter: '~',
-      cacheGroups: {
-        defaultVendors: {
-          test: /[\\/]node_modules[\\/]/,
-          priority: -10,
-        },
-        default: {
-          minChunks: 2,
-          priority: -20,
-          reuseExistingChunk: true,
-        },
-      },
-    },
-  },
+
   devtool: 'cheap-eval-source-map',
 };
--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,38 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common.js');
-
+const TerserPlugin = require('terser-webpack-plugin');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 module.exports = merge(common, {
   mode: 'production',
+  optimization: {
+    minimizer: [
+      new UglifyJSPlugin({
+        uglifyOptions: {
+          compress: {
+            drop_console: true,
+          },
+        },
+      }),
+    ],
+    splitChunks: {
+      chunks: 'async',
+      minSize: 30000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 6,
+      maxInitialRequests: 4,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 });
